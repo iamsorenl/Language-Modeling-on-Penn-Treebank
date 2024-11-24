404b872c6b87d8487a6a0840a48f25c9a5f12f8d
Soren Larsen <iamsorenl@gmail.com>
2024-11-23 17:29:33 -0800

    more

--- model.py
+++ model.py
@@ -44,3 +44,18 @@ class PositionalEncoding(nn.Module):
     def forward(self, x):
         x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False)
         return self.dropout(x)
+
+class LayerNormalization(nn.Module):
+    '''
+    This module defines the LayerNormalization class, which is responsible for normalizing the input tensor across the last dimension.
+    '''
+    def __inite__(self, eps: float = 10**-6) -> None:
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(1)) # Multiplied
+        self.bias = nn.Parameter(torch.zeros(1)) # Added
+
+    def forward(self, x):
+        mean = x.mean(dim = -1, keepdim=True)
+        std = x.std(dim = -1, keepdim=True)
+        return self.alpha * (x - mean) / (std + self.eps) + self.bias
