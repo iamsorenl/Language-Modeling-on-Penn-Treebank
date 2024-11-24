b71dafb6aa0f1a0acc2ccbd44ba133747187d2e5
Soren Larsen <iamsorenl@gmail.com>
2024-11-24 00:17:54 -0800

    update

--- model.py
+++ model.py
@@ -1,3 +1,5 @@
+# This model follows the transformer paper by Vaswani et al. (2017) and implements the transformer architecture using PyTorch.
+
 import torch
 import torch.nn as nn
 import math
@@ -59,3 +61,77 @@ class LayerNormalization(nn.Module):
         mean = x.mean(dim = -1, keepdim=True)
         std = x.std(dim = -1, keepdim=True)
         return self.alpha * (x - mean) / (std + self.eps) + self.bias
+
+class FeedForwardBlock(nn.Module):
+    '''
+    This module defines the FeedForwardBlock class, which is responsible for applying a feedforward neural network to the input tensor.
+    '''
+    def __init__(self, d_model: int, d_ff: int, dropout:float) -> None:
+        super().__init__()
+        self.linear1 = nn.Linear(d_model, d_ff) # W1 and B1
+        self.dropout = nn.Dropout(dropout)
+        self.linear2 = nn.Linear(d_ff, d_model) # W2 and B2
+
+    def forward(self, x):
+        # (Batch, seq_len, d_model) --> (Batch, seq_len, d_ff) --> (Batch, seq_len, d_model)
+        return self.linear2(self.dropout(torch.relu(self.linear1(x))))
+    
+class MultiHeadAttention(nn.Module):
+
+    def __init__(self, d_model: int, h: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.h = h
+        assert d_model % h == 0, 'd_model must be divisible by h'
+
+        self.d_k = d_model // h
+        self.w_q = nn.Linear(d_model, d_model) # W_q
+        self.w_k = nn.Linear(d_model, d_model) # W_k
+        self.w_v = nn.Linear(d_model, d_model) # W_v
+
+        self.w_o = nn.Linear(d_model, d_model) # W_o
+        self.dropout = nn.Dropout(dropout)
+
+    @staticmethod
+    def attention(query, key, value, mask, dropout: nn.Droput):
+        d_k = query.shape[-1]
+
+        # (Batch, h, seq_len, d_k) x (Batch, h, d_k, seq_len) --> (Batch, h, seq_len, seq_len)
+        attention_scores = (query @ key.transpose(-2, -1)) / math.sqrt(d_k)
+        if mask is not None:
+            attention_scores.masked_fill(mask == 0, -1e9)
+        attention_scores = torch.softmax(attention_scores, dim = -1) # (Batche, h, seq_len, seq_len)
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+
+        return (attention_scores @ value), attention_scores
+
+    def forward(self, q, k, v, mask):
+        query = self.w_q(q) # (Batch, seq_len, d_model) --> (Batch, seq_len, d_model)
+        key = self.w_k(k) # (Batch, seq_len, d_model) --> (Batch, seq_len, d_model)
+        value = self.w_v(v) # (Batch, seq_len, d_model) --> (Batch, seq_len, d_model)
+
+        # (Batch, seq_len, d_model) --> (Batch, seq_len, h, d_k) --> (Batch, h, seq_len, d_k)
+        query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1, 2)
+        key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1, 2)
+        value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1, 2)
+
+        x, self.attention_scores = MultiHeadAttention.attention(query, key, value, mask, self.dropout)
+
+        # (Batch, h, seq_len, d_k) --> (Batch, seq_len, h, d_k) --> (Batch, seq_len, d_model)
+        x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.h * self.d_k)
+
+        # (Batch, seq_len, d_model) --> (Batch, seq_len, d_model)
+        return self.w_o(x)
+
+class ResidualConnection(nn.Module):
+    '''
+    This module defines the ResidualConnection class, which is responsible for adding the input tensor to the output tensor.
+    '''
+    def __init__(self, dropout: float) -> None:
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
+        self.norm = LayerNormalization()
+
+    def forward(self, x, sublayer):
+        return x + self.dropout(sublayer(self.norm(x)))
